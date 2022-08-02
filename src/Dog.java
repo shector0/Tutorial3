6b196988be50747a6d5146927c25fc9566c6b80d
Stuart <stuart.hector@gmail.com>
2022-08-02 23:07:52 +1000

    Tutorial 3 begins

--- src/Dog.java
+++ src/Dog.java
@@ -4,15 +4,29 @@ public class Dog {
     private final long microchipNumber;
     private final String breed;
     private final char sex;
-    private final int age;
     private boolean deSexed;
+    private final int age;
 
-    public Dog(String name, long microchipNumber, String breed, char sex, int age, boolean deSexed) {
+    /**
+     * Contstructor Class for Dog related fields
+     * @param name The name of the dog
+     * @param microchipNumber a 9 digit unique number
+     * @param breed the dog breed
+     * @param sex the dog's gender
+     * @param deSexed desexed or not
+     * @param age the dog age
+     */
+    public Dog(String name, long microchipNumber, String breed, char sex, boolean deSexed, int age) {
         this.name = name;
         this.microchipNumber = microchipNumber;
         this.breed = breed;
         this.sex = sex;
+        this.deSexed = deSexed;
         this.age = age;
+    }
+
+    // *---- Setters and Getters ----*
+    public void setDeSexed(boolean deSexed) {
         this.deSexed = deSexed;
     }
 
@@ -32,32 +46,13 @@ public class Dog {
         return sex;
     }
 
-    public int getAge() {
-        return age;
-    }
-
     public boolean isDeSexed() {
         return deSexed;
     }
 
-    public void setDeSexed(boolean deSexed) {
-        this.deSexed = deSexed;
+    public int getAge() {
+        return age;
     }
-
-    public boolean sameBreed(Dog dog){
-        return breed.equals(dog.getBreed());
-    }
-
-    public boolean sameSex(Dog dog){
-        return sex=dog.getSex();
-    }
-
-    public boolean withingAgeRange(int min, int max){
-        return this.age>=min && this.age<=max;
-    }
-
-    public boolean sameDeSexed(Dog dog){
-        return this.deSexed ==(dog.isDeSexed());
-    }
-
 }
+
+
